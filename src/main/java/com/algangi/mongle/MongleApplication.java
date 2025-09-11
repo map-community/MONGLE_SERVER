21f4d696d7ec5648e1c19296b66161a87aa279dd
lwj0831 <wjl0831@gmail.com>
2025-09-11 21:17:25 +0900

    chore: 스프링 프로젝트 초기 설정 및 의존성 추가

---
+++ src/main/java/com/algangi/mongle/MongleApplication.java
@@ -0,0 +1,13 @@
+package com.algangi.mongle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algangi/mongle/MongleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algangi.mongle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
