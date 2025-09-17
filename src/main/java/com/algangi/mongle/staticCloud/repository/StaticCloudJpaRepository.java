67662e2f92d85b4923b2d5df63dab8864336c28b
KIYEONG <rldud1237@naver.com>
2025-09-17 12:11:02 +0900

    feat(s2): staticCloud repository에 이름 기반 조회 추가

--- src/main/java/com/algangi/mongle/staticCloud/repository/StaticCloudJpaRepository.java
+++ src/main/java/com/algangi/mongle/staticCloud/repository/StaticCloudJpaRepository.java
@@ -1,8 +1,10 @@
 package com.algangi.mongle.staticCloud.repository;
 
 import com.algangi.mongle.staticCloud.domain.StaticCloud;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StaticCloudJpaRepository extends JpaRepository<StaticCloud, Long> {
 
+    Optional<StaticCloud> findByName(String name);
 }
