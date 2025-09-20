d3e558e79a007aca8ee9afe9e0f74cd8441873d6
lwj0831 <wjl0831@gmail.com>
2025-09-20 18:40:41 +0900

    chore(dynamicCloud): DynamicCloudCells 엔티티 삭제 및 밸류타입으로 리팩토링

--- src/main/java/com/algangi/mongle/dynamicCloud/domain/model/DynamicCloud.java
+++ src/main/java/com/algangi/mongle/dynamicCloud/domain/model/DynamicCloud.java
@@ -1,19 +1,21 @@
-package com.algangi.mongle.dynamicCloud.domain;
+package com.algangi.mongle.dynamicCloud.domain.model;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import com.algangi.mongle.global.entity.CreatedDateBaseEntity;
-import com.algangi.mongle.s2.domain.DynamicCloudS2Cell;
-import jakarta.persistence.CascadeType;
+
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -38,25 +40,14 @@ public class DynamicCloud extends CreatedDateBaseEntity {
     @Enumerated(EnumType.STRING)
     private DynamicCloudStatus dynamicCloudStatus = DynamicCloudStatus.ACTIVE;
 
-    @OneToMany(mappedBy = "dynamicCloud", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @Builder.Default
-    private List<DynamicCloudS2Cell> s2Cells = new ArrayList<>();
+    @ElementCollection
+    @CollectionTable(name = "s2_dynamic_cloud_cells",
+        joinColumns = @JoinColumn(name = "s2_token_id", unique = true)
+    )
+    private Set<String> s2TokenIds;
 
     public static DynamicCloud createDynamicCloudFromTokens(List<String> s2TokenIds) {
-        DynamicCloud dc = DynamicCloud.builder().build();
-        s2TokenIds.forEach(dc::addS2CellToken);
-        return dc;
+        return DynamicCloud.builder().build();
     }
 
-    public void addS2Cells(List<DynamicCloudS2Cell> s2Cells) {
-        s2Cells.forEach(this::addS2Cell);
-    }
-
-    public void addS2CellToken(String s2TokenId) {
-        this.s2Cells.add(DynamicCloudS2Cell.of(s2TokenId, this));
-    }
-
-    public void addS2Cell(DynamicCloudS2Cell s2Cell) {
-        this.s2Cells.add(s2Cell);
-    }
 }
--- src/main/java/com/algangi/mongle/s2/repository/DynamicCloudS2CellJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.algangi.mongle.s2.repository;
-
-import com.algangi.mongle.s2.domain.DynamicCloudS2Cell;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DynamicCloudS2CellJpaRepository extends JpaRepository<DynamicCloudS2Cell, String> {
-
-}
