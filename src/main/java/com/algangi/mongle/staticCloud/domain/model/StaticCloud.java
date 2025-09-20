53f814bad2206fc639f929d48202d8cbea5d14d2
lwj0831 <wjl0831@gmail.com>
2025-09-20 18:31:26 +0900

    refactor(staticCloud): staticCloudCells 엔티티 삭제 및 밸류타입으로 리팩토링

--- src/main/java/com/algangi/mongle/staticCloud/domain/model/StaticCloud.java
+++ src/main/java/com/algangi/mongle/staticCloud/domain/model/StaticCloud.java
@@ -1,10 +1,15 @@
-package com.algangi.mongle.staticCloud.domain;
+package com.algangi.mongle.staticCloud.domain.model;
 
+import java.util.Set;
+
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
@@ -40,11 +45,18 @@ public class StaticCloud {
     @Column(nullable = false)
     private Double longitude;
 
-    public static StaticCloud createStaticCloud(String name, Double latitude, Double longitude) {
+    @ElementCollection
+    @CollectionTable(name = "s2_static_cloud_cells",
+        joinColumns = @JoinColumn(name = "s2_token_id", unique = true)
+    )
+    private Set<String> s2TokenIds;
+
+    public static StaticCloud createStaticCloud(String name, Double latitude, Double longitude, Set<String> s2TokenIds) {
         return StaticCloud.builder()
             .name(name)
             .latitude(latitude)
             .longitude(longitude)
+            .s2TokenIds(s2TokenIds)
             .build();
     }
 }
--- src/main/java/com/algangi/mongle/staticCloud/repository/StaticCloudRepository.java
+++ src/main/java/com/algangi/mongle/staticCloud/repository/StaticCloudRepository.java
@@ -1,10 +1,12 @@
 package com.algangi.mongle.staticCloud.repository;
 
-import com.algangi.mongle.staticCloud.domain.StaticCloud;
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StaticCloudJpaRepository extends JpaRepository<StaticCloud, Long> {
+import com.algangi.mongle.staticCloud.domain.model.StaticCloud;
+
+public interface StaticCloudRepository extends JpaRepository<StaticCloud, Long> {
 
     Optional<StaticCloud> findByName(String name);
 }
