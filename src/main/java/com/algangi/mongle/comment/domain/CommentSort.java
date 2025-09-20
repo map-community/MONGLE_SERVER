a6e1d77c95367ee64f287749f8967260fdbbcf83
HJH <jijinny26@gmail.com>
2025-09-21 06:46:14 +0900

    feat(comment): CommentSort enum 정렬 기준 추가

--- src/main/java/com/algangi/mongle/comment/domain/CommentSort.java
+++ src/main/java/com/algangi/mongle/comment/domain/CommentSort.java
@@ -1,5 +1,22 @@
 package com.algangi.mongle.comment.domain;
 
+import com.querydsl.core.types.OrderSpecifier;
+import static com.algangi.mongle.comment.domain.QComment.comment;
+
 public enum CommentSort {
-    LATEST, LIKES
+    LATEST, LIKES;
+
+    public OrderSpecifier<?>[] getOrderSpecifiers() {
+        return switch (this) {
+            case LIKES -> new OrderSpecifier[]{
+                    comment.likeCount.desc(),
+                    comment.createdDate.desc(),
+                    comment.id.desc()
+            };
+            case LATEST -> new OrderSpecifier[]{
+                    comment.createdDate.desc(),
+                    comment.id.desc()
+            };
+        };
+    }
 }
