2e363f05192718d77865d1bfa4ad9d2ab0e3eb1d
HJH <jijinny26@gmail.com>
2025-09-22 16:55:16 +0900

    refactor: 불필요한 @RequiredArgsConstructor 제거

--- src/main/java/com/algangi/mongle/comment/domain/service/CommentDomainService.java
+++ src/main/java/com/algangi/mongle/comment/domain/service/CommentDomainService.java
@@ -3,11 +3,9 @@ package com.algangi.mongle.comment.domain.service;
 import com.algangi.mongle.comment.domain.model.Comment;
 import com.algangi.mongle.member.domain.Member;
 import com.algangi.mongle.post.domain.Post;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class CommentDomainService {
 
     public Comment createParentComment(Post post, Member author, String content) {
