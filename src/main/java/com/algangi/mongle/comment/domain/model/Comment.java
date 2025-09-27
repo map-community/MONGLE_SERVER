ab77ceedb5ed0ff6a66a59bbae118aeb1042360f
HJH <jijinny26@gmail.com>
2025-09-27 23:23:32 +0900

    refactor(member, comment): 엔티티 PK를 TSID로 변경

--- src/main/java/com/algangi/mongle/comment/domain/model/Comment.java
+++ src/main/java/com/algangi/mongle/comment/domain/model/Comment.java
@@ -6,11 +6,10 @@ import com.algangi.mongle.global.exception.ApplicationException;
 import com.algangi.mongle.member.domain.Member;
 import com.algangi.mongle.post.domain.model.Post;
 
+import io.hypersistence.utils.hibernate.id.Tsid;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -27,12 +26,12 @@ import java.time.LocalDateTime;
 @Table(name = "comment")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
+@Builder(access = AccessLevel.PRIVATE)
 @Getter
 public class Comment extends TimeBaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Tsid
     private Long id;
 
     @Column(nullable = false)
