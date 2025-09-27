511a9966d595d27826fa00a4c6f6be35511a3480
lwj0831 <wjl0831@gmail.com>
2025-09-27 19:17:29 +0900

    chore(post): PostFileErrorCode INVALID_FILE_COUNT message 수정

--- src/main/java/com/algangi/mongle/post/exception/PostFileErrorCode.java
+++ src/main/java/com/algangi/mongle/post/exception/PostFileErrorCode.java
@@ -11,7 +11,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum PostFileErrorCode implements ErrorCode {
 
-    INVALID_FILE_COUNT(HttpStatus.BAD_REQUEST, "FILE-001", "이미지 파일의 총 용량이 최대 허용치를 초과했습니다."),
+    INVALID_FILE_COUNT(HttpStatus.BAD_REQUEST, "FILE-001", "업로드할 수 있는 파일 개수를 초과했습니다."),
     INVALID_VIDEO_COUNT(HttpStatus.BAD_REQUEST, "FILE-002", "업로드할 수 있는 동영상의 최대 개수를 초과했습니다."),
     INVALID_TOTAL_IMAGE_SIZE(HttpStatus.BAD_REQUEST, "FILE-003", "이미지 파일의 총 용량이 최대 허용치를 초과했습니다."),
     INVALID_VIDEO_SIZE(HttpStatus.BAD_REQUEST, "FILE-004", "업로드할 수 있는 동영상의 최대 사이즈를 초과했습니다."),
