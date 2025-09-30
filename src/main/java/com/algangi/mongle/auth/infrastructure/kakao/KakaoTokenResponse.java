3483f36f4edb543bd2bfe9777082d5a78b5bf72d
lwj0831 <wjl0831@gmail.com>
2025-10-01 01:08:55 +0900

    chore(auth): 웹 기반 방식에서 안드로이드 기반 json 응답 방식으로 리팩토링을 위한 불필요한 파일 제거

--- src/main/java/com/algangi/mongle/auth/infrastructure/kakao/KakaoTokenResponse.java
+++
@@ -1,9 +0,0 @@
-package com.algangi.mongle.auth.infrastructure.kakao;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record KakaoTokenResponse(
-    @JsonProperty("access_token") String accessToken
-) {
-
-}
