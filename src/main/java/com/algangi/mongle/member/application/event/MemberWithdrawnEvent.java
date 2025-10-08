13ee7bdb4dc4cbf7ffa6b76b4d14f21819507945
HJH <jijinny26@gmail.com>
2025-10-08 21:31:56 +0900

    refactor: 데이터 정합성을 위해 멤버에 탈퇴 상테 추가

--- src/main/java/com/algangi/mongle/member/application/event/MemberWithdrawnEvent.java
+++ src/main/java/com/algangi/mongle/member/application/event/MemberWithdrawnEvent.java
@@ -1,15 +1,7 @@
 package com.algangi.mongle.member.application.event;
 
-import com.algangi.mongle.stats.application.dto.ReactionCleanupDto;
-
-import java.util.List;
-import java.util.Map;
-
 public record MemberWithdrawnEvent(
-        String memberId,
-        List<String> postIds,
-        List<String> commentIds,
-        List<ReactionCleanupDto> reactions,
-        Map<String, Long> postCommentCountDelta,
-        Map<String, List<String>> commentsByPost
-) {}
+        String memberId
+) {
+
+}
--- src/main/java/com/algangi/mongle/member/application/service/WithdrawalCleanupService.java
+++ src/main/java/com/algangi/mongle/member/application/service/WithdrawalCleanupService.java
@@ -1,13 +1,21 @@
 package com.algangi.mongle.member.application.service;
 
+import com.algangi.mongle.comment.domain.model.Comment;
 import com.algangi.mongle.comment.domain.repository.CommentRepository;
 import com.algangi.mongle.member.application.event.MemberWithdrawnEvent;
 import com.algangi.mongle.member.repository.MemberRepository;
+import com.algangi.mongle.post.domain.repository.PostRepository;
 import com.algangi.mongle.reaction.domain.repository.ReactionRepository;
+import com.algangi.mongle.reaction.infrastructure.persistence.ReactionRepositoryCustom;
+import com.algangi.mongle.stats.application.dto.ReactionCleanupDto;
 import com.algangi.mongle.stats.application.service.ContentStatsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,32 +25,42 @@ public class WithdrawalCleanupService {
     private final ContentStatsService contentStatsService;
     private final MemberRepository memberRepository;
     private final ReactionRepository reactionRepository;
+    private final PostRepository postRepository;
     private final CommentRepository commentRepository;
+    private final ReactionRepositoryCustom reactionRepositoryCustom;
     private final ContentManagementDbService dbService;
 
-    // 사용자 프로필 삭제, 소셜 계정 연동 삭제 등 추가 필요
+    // 사용자 프로필 삭제, 소셜 계정 연동 삭제, 세션/토큰 무효화 처리 필요
     @Transactional
     public void cleanupDataFor(MemberWithdrawnEvent event) {
+        String memberId = event.memberId();
+
+        // 필요한 테이터 조회
+        List<String> postIds = postRepository.findAllIdsByMemberId(memberId);
+        List<ReactionCleanupDto> reactions = reactionRepositoryCustom.findAllReactionCleanupData(memberId);
+        List<Comment> comments = commentRepository.findAllByMember_MemberId(memberId);
+        List<String> commentIds = comments.stream().map(Comment::getId).toList();
+
+        // 댓글 처리에 필요한 Map 가공
+        Map<String, Long> postCommentCountDelta = comments.stream()
+                .collect(Collectors.groupingBy(c -> c.getPost().getId(), Collectors.counting()));
+        Map<String, List<String>> commentsByPost = comments.stream()
+                .collect(Collectors.groupingBy(c -> c.getPost().getId(), Collectors.mapping(Comment::getId, Collectors.toList())));
+
         // 리액션 정리
-        contentStatsService.removeReactionsFromRedis(event.memberId(), event.reactions());
-        reactionRepository.deleteAllByMemberId(event.memberId());
+        contentStatsService.removeReactionsFromRedis(memberId, reactions);
+        reactionRepository.deleteAllByMemberId(memberId);
 
         // 게시글, 댓글 정리
-        contentManagementService.cleanupRedisDataForPosts(event.postIds());
-        contentManagementService.cleanupRedisDataForComments(
-                event.commentIds(),
-                event.postCommentCountDelta(),
-                event.commentsByPost()
-        );
+        contentManagementService.cleanupRedisDataForPosts(postIds);
+        contentManagementService.cleanupRedisDataForComments(commentIds, postCommentCountDelta, commentsByPost);
 
-        dbService.updateWithdrawnUserCommentsInDb(event.commentIds(),
-                event.postCommentCountDelta());
-        dbService.updateWithdrawnUserPostsInDb(event.postIds());
-
-        // postRepository.unlinkMemberFromPosts(event.memberId());
-        commentRepository.unlinkMemberFromComments(event.memberId());
+        dbService.updateWithdrawnUserCommentsInDb(commentIds, postCommentCountDelta);
+        dbService.updateWithdrawnUserPostsInDb(postIds);
+        // postRepository.unlinkMemberFromPosts(memberId);
+        commentRepository.unlinkMemberFromComments(memberId);
 
         // 멤버 엔티티 삭제
-        memberRepository.deleteById(event.memberId());
+        memberRepository.deleteById(memberId);
     }
 }
