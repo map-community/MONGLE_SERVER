1688bba2314fab0693f54ef9b7f55393cd91498e
lwj0831 <wjl0831@gmail.com>
2025-10-16 17:00:20 +0900

    refactor(file): 업로드용 url 발급 시 FileKey sanitize하는 로직 추가

--- src/main/java/com/algangi/mongle/file/domain/FileHandler.java
+++ src/main/java/com/algangi/mongle/file/domain/FileHandler.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import com.algangi.mongle.file.application.dto.FileMetadata;
 import com.algangi.mongle.file.presentation.dto.UploadUrlRequest.UploadFileInfo;
+import com.google.common.io.Files;
 
 public interface FileHandler {
+
+    int MAX_FILENAME_LENGTH = 50;
 
     FileType getFileType();
 
@@ -14,4 +17,18 @@ public interface FileHandler {
     String generateFileKey(String fileName);
 
     List<FileMetadata> createMetadata(List<UploadFileInfo> fileInfos);
+
+    default String sanitizeFileName(String originalFileName) {
+        String name = Files.getNameWithoutExtension(originalFileName);
+        String extension = Files.getFileExtension(originalFileName).toLowerCase();
+
+        String sanitized = name.toLowerCase().replaceAll("\\s+", "-");
+        sanitized = sanitized.replaceAll("[^a-z0-9-_]", "");
+        sanitized = sanitized.replaceAll("--+", "-");
+
+        if (sanitized.length() > MAX_FILENAME_LENGTH) {
+            sanitized = sanitized.substring(0, MAX_FILENAME_LENGTH);
+        }
+        return sanitized + "." + extension;
+    }
 }
